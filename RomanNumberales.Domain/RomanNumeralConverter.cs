60b6710de748998ded65d3f99aab2bd0746ca665
Chekkan <harish_babu@outlook.com>
2014-03-28 14:19:22 +0000

    first kata session

---
+++ RomanNumberales.Domain/RomanNumeralConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace RomanNumerals.Domain
+{
+    public class RomanNumeralConverter
+    {
+        public string ConvertToNumeral(int number)
+        {
+            StringBuilder numeral = new StringBuilder();
+
+            double div = Convert.ToDouble(number) / 5;
+            int rem = number % 5;
+
+            if (div - Math.Floor(div) == 0.8)
+            {
+                AppendI(numeral, 1);
+                if (Math.Floor(div) % 2 == 0)
+                    numeral.Append("V");
+                else
+                    numeral.Append("X");
+            }
+            else if (div >= 0.7)
+            {
+                if (number >= 10)
+                    numeral.Append("X");
+                else
+                    numeral.Append("V");
+
+                AppendI(numeral, rem);
+            }
+            else
+            {
+                AppendI(numeral, number);
+            }
+
+            return numeral.ToString();
+        }
+
+        private void AppendI(StringBuilder numeral, int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                numeral.Append("I");
+            }
+        }
+    }
+}
---
+++ RomanNumerales.UnitTests/RomanNumeralConverterTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using RomanNumerals.Domain;
+using Xunit.Extensions;
+
+namespace RomanNumerals.UnitTests
+{
+    public class RomanNumeralConverterTest
+    {
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(11, "XI")]
+        [InlineData(12, "XII")]
+        [InlineData(13, "XIII")]
+        [InlineData(14, "XIV")]
+        public void ConvertReturnsExpected(int number, string numeral)
+        {
+            AssertEqual(number, numeral);
+        }
+
+        private void AssertEqual(int number, string expected)
+        {
+            RomanNumeralConverter sut = new RomanNumeralConverter();
+            string result = sut.ConvertToNumeral(number);
+            Assert.Equal(expected, result);
+        }
+    }
+}
